4c5c31a1e9a4c793821613901fc78c24f4da5e94
Prashant Parmar <prashantparmar9919@gmail.com>
2026-04-18 09:07:12 +0530

    chore: base state for route implementation

--- Assignment-1/src/routes/note.routes.js
+++ Assignment-1/src/routes/note.routes.js
@@ -2,16 +2,6 @@ const express = require('express');
 const router = express.Router();
 const noteController = require('../controllers/note.controller');
 
-// Bulk routes first to avoid conflict with /:id
-router.post('/bulk', noteController.createBulkNotes);
-router.delete('/bulk', noteController.deleteBulkNotes);
-
-// Single note routes
-router.get('/', noteController.getAllNotes);
-router.get('/:id', noteController.getNoteById);
-router.put('/:id', noteController.replaceNote);
-router.patch('/:id', noteController.updateNote);
-router.delete('/:id', noteController.deleteNote);
-router.post('/', noteController.createNote);
+// Routes will be added here
 
 module.exports = router;
