08cced22de202935cd415fc85241287fd6e7e8a5
Prashant Parmar <prashantparmar9919@gmail.com>
2026-04-18 09:08:16 +0530

    feat: add get note by id route

--- Assignment-1/src/routes/note.routes.js
+++ Assignment-1/src/routes/note.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const noteController = require('../controllers/note.controller');
 
 router.get('/', noteController.getAllNotes);
+router.get('/:id', noteController.getNoteById);
 router.post('/', noteController.createNote);
 
 module.exports = router;
